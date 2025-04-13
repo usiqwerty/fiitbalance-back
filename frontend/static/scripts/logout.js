2b32e9072579c19f6ee17ee3fed3b653a6a8bfbd
cynpy <stepan.grebnev.05@mail.ru>
2025-04-13 20:16:08 +0500

    fix logout

--- frontend/static/scripts/logout.js
+++ frontend/static/scripts/logout.js
@@ -1,21 +1,23 @@
-﻿async function logout() {
+﻿const apiBaseUrl = "http://127.0.0.1:8000";
+
+async function logout() {
     try {
-        const response = await fetch('/auth/logout', {
-            method: 'POST',
-            credentials: 'include'
+        const response = await fetch(`${apiBaseUrl}/api/auth/logout`, {
+            method: "POST",
+            credentials: "include"
         });
 
         if (response.ok) {
-            window.location.href = '/login';
+            window.location.href = "/login";
         } else {
-            console.error('Ошибка выхода:', await response.text());
+            console.error("Ошибка выхода:", await response.text());
         }
     } catch (error) {
-        console.error('Ошибка сети:', error);
-        window.location.href = '/login';
+        console.error("Ошибка сети:", error);
+        window.location.href = "/login";
     }
 }
 
-document.querySelectorAll('.logout-button').forEach(button => {
-    button.addEventListener('click', logout);
+document.querySelectorAll(".logout-button").forEach(button => {
+    button.addEventListener("click", logout);
 });
